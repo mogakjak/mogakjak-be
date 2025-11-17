d046c4352f0346c506bc777ff5b0db76e24a5de9
EunjinWoo <esther0904@naver.com>
2025-11-17 13:05:10 +0900

    chore: dto 설명 추가

--- src/main/java/com/mogakjak/mogakjak/domain/timer/dto/response/DailyFocusStatsResponse.java
+++ src/main/java/com/mogakjak/mogakjak/domain/timer/dto/response/DailyFocusStatsResponse.java
@@ -1,8 +1,15 @@
 package com.mogakjak.mogakjak.domain.timer.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
 public record DailyFocusStatsResponse(
+
+        @Schema(description = "날짜", example = "2025-11-17")
         LocalDate date,
+
+        @Schema(description = "몰입 시간(초 단위)", example = "197")
         Long totalSeconds
+
 ) {}
