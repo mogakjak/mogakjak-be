c3c9170fa6211f1cfbd21ceaab37e1f1d3abbc5e
EunjinWoo <esther0904@naver.com>
2025-11-17 13:20:48 +0900

    chore: dayOfWeek 부연 설명 추가

--- src/main/java/com/mogakjak/mogakjak/domain/timer/controller/FocusRecordController.java
+++ src/main/java/com/mogakjak/mogakjak/domain/timer/controller/FocusRecordController.java
@@ -24,7 +24,7 @@ public class FocusRecordController {
 
     private final FocusRecordService focusRecordService;
 
-    @Operation(summary = "해당 연도의 일일 기록 조회", description = "해당 연도의 1월 1일 - 12월 31일까지의 일일 기록을 일자 별로 초 단위로 계산하여 반환합니다.")
+    @Operation(summary = "해당 연도의 일일 기록 조회", description = "해당 연도의 1월 1일 - 12월 31일까지의 일일 기록을 일자 별로 초 단위로 계산하여 반환합니다. dayOfWeek은 요일로, 1(월)-7(일)을 의미합니다.")
     @GetMapping("/statistics/daily")
     public ApiResponse<List<DailyFocusStatsResponse>> getDailyStatistics(
             @Parameter(hidden = true) @CurrentUser User user
