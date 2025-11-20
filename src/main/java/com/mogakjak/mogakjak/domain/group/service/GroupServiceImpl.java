51589e0410a272a927967858f5318779eeff6780
Joon9750 <junhyeok2166@daum.net>
2025-11-21 02:06:55 +0900

    fix: transactional 수정

--- src/main/java/com/mogakjak/mogakjak/domain/group/service/GroupServiceImpl.java
+++ src/main/java/com/mogakjak/mogakjak/domain/group/service/GroupServiceImpl.java
@@ -28,7 +28,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class GroupServiceImpl implements GroupService {
 
     private final UserRepository userRepository;
