eeb9b8887962051c36cef73fc68ece73e50fe6f4
EunjinWoo <esther0904@naver.com>
2025-11-27 10:22:42 +0900

    fix: PUT -> GET으로 수정

--- src/main/java/com/mogakjak/mogakjak/domain/group/controller/GroupController.java
+++ src/main/java/com/mogakjak/mogakjak/domain/group/controller/GroupController.java
@@ -94,7 +94,7 @@ public class GroupController {
     }
 
     @Operation(summary = "그룹 집중 체크 알림 설정", description = "그룹의 집중 체크 알림 동의 여부 / 알림 주기 / 알림 메시지를 설정합니다.")
-    @PutMapping("/{groupId}/notifications")
+    @GetMapping("/{groupId}/notifications")
     public ApiResponse<FocusNotificationResponse> getFocusNotification(
             @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable UUID groupId
